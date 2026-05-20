992fe67a7494827cc75061d08058e4731ed08f51
unknown <zuzia.myslek@gmail.com>
2026-05-20 02:18:42 +0200

    life dev

--- Assets/Life System/Scripts/LifeWorldIndicator.cs
+++ Assets/Life System/Scripts/LifeWorldIndicator.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 namespace TechJuego.LifeSystem
 {
@@ -7,11 +8,13 @@ namespace TechJuego.LifeSystem
     {
         [SerializeField] private string ProfileId = "";
         [SerializeField] private float VerticalOffset = 2.0f;
-        [SerializeField] private Vector2 PanelSize = new Vector2(180.0f, 72.0f);
+        [SerializeField] private Vector2 PanelSize = new Vector2(180.0f, 40.0f);
+        [SerializeField] private float HeartSize = 22.0f;
+        [SerializeField] private float HeartSpacing = 2.0f;
 
         private Transform target;
-        private Text lifeCountText;
-        private Text remainingText;
+        private RectTransform heartsRoot;
+        private readonly List<GameObject> heartObjects = new List<GameObject>();
         private bool initialized;
 
         public void Initialize(string profileId, Transform targetTransform, float verticalOffset)
@@ -63,7 +66,7 @@ namespace TechJuego.LifeSystem
                 return;
             }
 
-            UpdateText(lifecount, remainingTime);
+            UpdateHearts(lifecount);
         }
 
         private void BuildIfNeeded()
@@ -120,7 +123,7 @@ namespace TechJuego.LifeSystem
 
             VerticalLayoutGroup layout = gameObject.AddComponent<VerticalLayoutGroup>();
             layout.childAlignment = TextAnchor.MiddleCenter;
-            layout.spacing = 2.0f;
+            layout.spacing = 0.0f;
             layout.padding = new RectOffset(8, 8, 6, 6);
             layout.childControlWidth = true;
             layout.childControlHeight = true;
@@ -131,90 +134,79 @@ namespace TechJuego.LifeSystem
             fitter.horizontalFit = ContentSizeFitter.FitMode.PreferredSize;
             fitter.verticalFit = ContentSizeFitter.FitMode.PreferredSize;
 
-            GameObject upperRow = new GameObject("UpperRow", typeof(RectTransform), typeof(HorizontalLayoutGroup), typeof(LayoutElement));
-            upperRow.transform.SetParent(transform, false);
+            GameObject heartsObject = new GameObject("Hearts", typeof(RectTransform), typeof(HorizontalLayoutGroup), typeof(LayoutElement));
+            heartsObject.transform.SetParent(transform, false);
 
-            HorizontalLayoutGroup rowLayout = upperRow.GetComponent<HorizontalLayoutGroup>();
+            heartsRoot = heartsObject.GetComponent<RectTransform>();
+
+            HorizontalLayoutGroup rowLayout = heartsObject.GetComponent<HorizontalLayoutGroup>();
             rowLayout.childAlignment = TextAnchor.MiddleCenter;
-            rowLayout.spacing = 6.0f;
+            rowLayout.spacing = HeartSpacing;
             rowLayout.childControlWidth = false;
-            rowLayout.childControlHeight = true;
+            rowLayout.childControlHeight = false;
             rowLayout.childForceExpandWidth = false;
             rowLayout.childForceExpandHeight = false;
 
-            GameObject heart = CreateHeartObject(upperRow.transform);
-            LayoutElement heartElement = heart.AddComponent<LayoutElement>();
-            heartElement.preferredWidth = 28.0f;
-            heartElement.preferredHeight = 28.0f;
+            LayoutElement rowElement = heartsObject.GetComponent<LayoutElement>();
+            rowElement.preferredHeight = HeartSize;
 
-            GameObject countObject = CreateTextObject("Count", upperRow.transform, "0", 18, Color.white, FontStyle.Bold, TextAnchor.MiddleLeft);
-            LayoutElement countElement = countObject.AddComponent<LayoutElement>();
-            countElement.preferredWidth = 42.0f;
-
-            remainingText = CreateTextObject("Time", transform, "Full", 14, new Color(0.88f, 0.9f, 1.0f, 1.0f), FontStyle.Normal, TextAnchor.MiddleCenter).GetComponent<Text>();
-            LayoutElement remainingElement = remainingText.gameObject.AddComponent<LayoutElement>();
-            remainingElement.preferredHeight = 18.0f;
-
-            lifeCountText = countObject.GetComponent<Text>();
             transform.name = string.IsNullOrEmpty(ProfileId) ? "LifeWorldIndicator" : ProfileId + "LifeIndicator";
         }
 
-        private GameObject CreateHeartObject(Transform parent)
+        private GameObject CreateHeartObject(Transform parent, int index)
         {
-            GameObject heartPrefab = Resources.Load<GameObject>("Prefabs/HeartContainer");
-            if (heartPrefab != null)
-            {
-                GameObject heart = Instantiate(heartPrefab, parent, false);
-                heart.name = "Heart";
-                RectTransform rect = heart.GetComponent<RectTransform>();
-                if (rect != null)
-                {
-                    rect.sizeDelta = new Vector2(28.0f, 28.0f);
-                }
+            GameObject heart = new GameObject("Heart" + index, typeof(RectTransform), typeof(CanvasRenderer), typeof(Image));
+            heart.transform.SetParent(parent, false);
 
-                return heart;
-            }
+            RectTransform rect = heart.GetComponent<RectTransform>();
+            rect.sizeDelta = new Vector2(HeartSize, HeartSize);
+            rect.localScale = Vector3.one;
 
-            GameObject fallback = new GameObject("Heart", typeof(RectTransform), typeof(CanvasRenderer), typeof(Image));
-            fallback.transform.SetParent(parent, false);
-            Image image = fallback.GetComponent<Image>();
+            Image image = heart.GetComponent<Image>();
             image.sprite = Resources.Load<Sprite>("Images/LifeSystem/red-heart");
             image.preserveAspect = true;
-            return fallback;
-        }
+            image.raycastTarget = false;
 
-        private GameObject CreateTextObject(string objectName, Transform parent, string text, int fontSize, Color color, FontStyle fontStyle, TextAnchor alignment)
-        {
-            GameObject textObject = new GameObject(objectName, typeof(RectTransform), typeof(CanvasRenderer), typeof(Text));
-            textObject.transform.SetParent(parent, false);
-
-            Text uiText = textObject.GetComponent<Text>();
-            uiText.text = text;
-            uiText.font = Resources.GetBuiltinResource<Font>("LegacyRuntime.ttf");
-            uiText.fontSize = fontSize;
-            uiText.fontStyle = fontStyle;
-            uiText.color = color;
-            uiText.alignment = alignment;
-            uiText.horizontalOverflow = HorizontalWrapMode.Overflow;
-            uiText.verticalOverflow = VerticalWrapMode.Overflow;
-            uiText.raycastTarget = false;
-
-            RectTransform rect = textObject.GetComponent<RectTransform>();
-            rect.sizeDelta = new Vector2(0.0f, 24.0f);
-
-            return textObject;
-        }
-
-        private void UpdateText(int lifeCount, string remainingTime)
-        {
-            if (lifeCountText != null)
+            if (image.sprite == null)
             {
-                lifeCountText.text = lifeCount.ToString();
+                image.enabled = false;
             }
 
-            if (remainingText != null)
+            return heart;
+        }
+
+        private void UpdateHearts(int lifeCount)
+        {
+            if (heartsRoot == null)
             {
-                remainingText.text = remainingTime;
+                return;
+            }
+
+            while (heartObjects.Count < lifeCount)
+            {
+                GameObject heart = CreateHeartObject(heartsRoot, heartObjects.Count);
+                heartObjects.Add(heart);
+            }
+
+            while (heartObjects.Count > lifeCount)
+            {
+                GameObject heart = heartObjects[heartObjects.Count - 1];
+                heartObjects.RemoveAt(heartObjects.Count - 1);
+                if (heart != null)
+                {
+                    Destroy(heart);
+                }
+            }
+
+            for (int i = 0; i < heartObjects.Count; i++)
+            {
+                GameObject heart = heartObjects[i];
+                if (heart == null)
+                {
+                    continue;
+                }
+
+                heart.SetActive(i < lifeCount);
             }
         }
 
@@ -227,7 +219,7 @@ namespace TechJuego.LifeSystem
 
             if (LifeHandler.Instance.TryGetLifeState(ProfileId, out int lifeCount, out string remainingTime))
             {
-                UpdateText(lifeCount, remainingTime);
+                UpdateHearts(lifeCount);
             }
         }
 
