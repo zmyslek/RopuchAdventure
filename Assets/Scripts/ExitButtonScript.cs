8d47fd426d331b910587e25333feda789eb746b3
unknown <zuzia.myslek@gmail.com>
2026-05-21 14:32:09 +0200

    saving changes to hand in

--- Assets/Scripts/ExitButtonScript.cs
+++ Assets/Scripts/ExitButtonScript.cs
@@ -8,21 +8,60 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ExitButtonScript : MonoBehaviour
 {
+        [SerializeField]
+        int sceneIndexToLoad = 0;
+
+    RectTransform buttonRect;
+    Canvas parentCanvas;
+    int lastActivationFrame = -1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        buttonRect = GetComponent<RectTransform>();
+        parentCanvas = GetComponentInParent<Canvas>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0) && IsPointerOverButton())
+        {
+            ActivateExit();
+        }
     }
 
     public void buttonClicked(){
         Debug.Log("Exit Button Clicked");
         ScoreState.Reset();
-        SceneManager.LoadScene(0);
+        ActivateExit();
+    }
+
+    void ActivateExit()
+    {
+        if (lastActivationFrame == Time.frameCount)
+        {
+            return;
+        }
+
+        lastActivationFrame = Time.frameCount;
+        ScoreState.Reset();
+        SceneManager.LoadScene(sceneIndexToLoad);
+    }
+
+    bool IsPointerOverButton()
+    {
+        if (buttonRect == null)
+        {
+            return false;
+        }
+
+        Camera eventCamera = null;
+        if (parentCanvas != null && parentCanvas.renderMode != RenderMode.ScreenSpaceOverlay)
+        {
+            eventCamera = parentCanvas.worldCamera;
+        }
+
+        return RectTransformUtility.RectangleContainsScreenPoint(buttonRect, Input.mousePosition, eventCamera);
     }
 }
--- Assets/Scripts/QuitButtonScript.cs
+++ Assets/Scripts/QuitButtonScript.cs
@@ -8,20 +8,60 @@ using UnityEngine;
 
 public class QuitButtonScript : MonoBehaviour
 {
+    RectTransform buttonRect;
+    Canvas parentCanvas;
+    int lastActivationFrame = -1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        buttonRect = GetComponent<RectTransform>();
+        parentCanvas = GetComponentInParent<Canvas>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0) && IsPointerOverButton())
+        {
+            QuitGame();
+        }
     }
 
     public void buttonClicked(){
         Debug.Log("Quit Button Clicked");
+        QuitGame();
+    }
+
+    void QuitGame()
+    {
+        if (lastActivationFrame == Time.frameCount)
+        {
+            return;
+        }
+
+        lastActivationFrame = Time.frameCount;
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
+    }
+
+    bool IsPointerOverButton()
+    {
+        if (buttonRect == null)
+        {
+            return false;
+        }
+
+        Camera eventCamera = null;
+        if (parentCanvas != null && parentCanvas.renderMode != RenderMode.ScreenSpaceOverlay)
+        {
+            eventCamera = parentCanvas.worldCamera;
+        }
+
+        return RectTransformUtility.RectangleContainsScreenPoint(buttonRect, Input.mousePosition, eventCamera);
     }
 }
